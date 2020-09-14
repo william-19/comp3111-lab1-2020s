7adab033ac743c0a6dff2fffd60ce79b68d9c23a
Christophorus William Wijaya <williwilliam1902@gmail.com>
2020-09-14 16:54:36 +0700

    Initial Project Setup

---
+++ src/main/java/Lab1/Library.java
@@ -0,0 +1,9 @@
+public class Library {
+	public static void main(String arg[]) {
+		System.out.println("When there is a fire, commit and push.");
+	}
+	
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
